377514a2b750dfc734154c0eced2bba61231caa1
syedzubeen <zubeenqadry@gmail.com>
2023-10-30 02:30:22 +0530

    Adds UI enhancements

--- app.py
+++ app.py
@@ -62,7 +62,7 @@ def upload_file():
             # Clear the file upload flag to allow re-uploading
             file_uploaded = False
 
-            return render_template('index.html', content=file_content, summarizer_response=summarizer_response)
+            return render_template('index.html',  summarizer_response=summarizer_response)
     
     return render_template('index.html', content=file_content, summarizer_response=None)
     
@@ -80,6 +80,13 @@ def read_docx(uploaded_file):
     for paragraph in doc.paragraphs:
         text += paragraph.text
     return text
+
+def chunk_text(text, chunk_size=2000):
+    chunks = []
+    for i in range(0, len(text), chunk_size):
+        chunk = text[i:i + chunk_size]
+        chunks.append(chunk)
+    return chunks
 
 def read_env_vars():
     current_file_directory = os.path.dirname(os.path.abspath(__file__))
@@ -108,24 +115,64 @@ async def summarizer(username, password, paper):
     
     input_string = paper
     cleaned_string = re.sub(r'[^a-zA-Z .]', '', input_string)
-    print(cleaned_string)
+    #print(cleaned_string)
 
-    custom_query = "SELECT answer FROM project_summary_gen.summaries WHERE question = 'You are a scientist and a researcher who understands scientific research papers and provide concise summaries of each paper. You aim is to generate a summary of the paper in 5-10 lines, capturing the key findings and contributions, followed by 5 key points from the paper presented in a bullet format. You should be able to handle research papers from various domains and adapt to the specific terminology and structure of the paper and should also prioritize the most relevant information and avoid excessive repetition in the summaries. The contents of the paper are long and I will send you the its contents in chunks and here is the first one: " + cleaned_string + "';"
-    resp = session.post('https://cloud.mindsdb.com/api/sql/query', json={'query': custom_query})
+    chunks = []
+    chunk_size=3200
+    summarised = ""
+    consolidated_summary = " "
+    for i in range(0, len(cleaned_string), chunk_size):
+        chunk_number = len(chunks) + 1
+        chunk =  cleaned_string[i:i + chunk_size]
+        chunks.append(chunk)
+
+
+    rate_affected_limit_chunk_size = 8
+    if chunk_number <8:
+        rate_affected_limit_chunk_size = chunk_number
+
+    # for i in range(rate_affected_limit_chunk_size):
+    #     print("\n\n\n\nCHUnk is here ", i)
+    #     print(chunks[i])
+
+    for i in range(rate_affected_limit_chunk_size-2):
+        custom_query = "SELECT answer FROM project_summary_gen.summaries WHERE question = 'You are a scientist and a researcher who understands scientific research papers and provide concise summaries of each paper. You aim is to generate a summary of a piece of research article in  stricly less than 50 words, capturing the key findings and contributions. You should handle research papers from various domains, adapt to the specific terminology and structure of the paper and should prioritize the most relevant information and avoid excessive repetition in the summaries. Here is a chunk of data summarise this: " + chunks[i] + "';"
+        resp = session.post('https://cloud.mindsdb.com/api/sql/query', json={'query': custom_query})
+
     
+
+
+        json_response = resp.json()
+        # print("\n\n\njson response is:\n")
+        # print (json_response)
+        # Assuming there's only one element in the inner list
+        summarised = json_response['data'][0][0]  
+        #print("summarised response is:\n")
+        #print (json_response)
+        # Remove special characters from start and end
+        summarised = summarised.strip('[\n]').strip()
+        #print("stripped response is:\n")
+        #print (json_response)
+        #Debug and print value
+        print("\n\n Summary generated is:")
+        print(summarised)
+
+        consolidated_summary = consolidated_summary + summarised
+        
+    print("\n\n CONSOLIDATED Summary generated is:")
+    print(consolidated_summary)
+
+    input_string = consolidated_summary
+    cleaned_complete_summary = re.sub(r'[^a-zA-Z .]', '', input_string)
+
+    custom_query = "SELECT answer FROM project_summary_gen.summaries WHERE question = 'You are a scientist and a researcher who understands scientific research papers and provide concise summaries of each paper. This is a collective article that contains summaries of different portions of a research paper. Consolidate all of these summaries into one single summary of 150 words in a bullet point format. " + cleaned_complete_summary + "';"
+    resp = session.post('https://cloud.mindsdb.com/api/sql/query', json={'query': custom_query})
+
+    print("\n\n SUMMARY OF SUMMARIES:")
     json_response = resp.json()
-    print("json response is:\n")
-    #print (json_response)
-    # Assuming there's only one element in the inner list
-    summarised = json_response['data'][0][0]  
-    #print("summarised response is:\n")
-    #print (json_response)
-    # Remove special characters from start and end
-    summarised = summarised.strip('[\n]').strip()
-    #print("stripped response is:\n")
-    #print (json_response)
-    #Debug and print value
-    print(summarised)
+    print("\n\n\njson response is:\n")
+    print (json_response)
+    
 
     if resp.status_code == 200:
         return summarised
